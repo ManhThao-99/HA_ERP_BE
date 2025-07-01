766d191668476bb05d538a28b239e2346dff7bb5
GiveMeHarderBattles <ngduyanh2812@gmail.com>
2025-07-01 19:23:23 +0700

    Fixed Error Codes names.

--- src/HA_ERP.Domain.Shared/HA_ERPDomainErrorCodes.cs
+++ src/HA_ERP.Domain.Shared/HA_ERPDomainErrorCodes.cs
@@ -4,8 +4,8 @@ public static class HA_ERPDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
 
-    public const string EntityAlreadyExists = "HAERP:20001";
-    public const string EntityNotFound = "HAERP:20002";
+    public const string EntityAlreadyExists = "HA_ERP:20001";
+    public const string EntityNotFound = "HA_ERP:20002";
 
 
     public const string OrganizationHasStaff = "HA_ERP:20003";
